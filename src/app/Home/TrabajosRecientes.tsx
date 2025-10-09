3edafdb97e8a1417c6e9386f23babbe8c1463ae7
Robert228-cod <robertlopezcaballero13@gmail.com>
2025-10-08 20:01:33 -0400

    implementando funcionalidades 14/26

--- src/app/Home/TrabajosRecientes.tsx
+++ src/app/Home/TrabajosRecientes.tsx
@@ -1,12 +1,26 @@
 'use client'
 
 import React, { useState } from 'react'
-import { JobCard } from '../../componets/JobCard'
 import jobs from '@/jsons/jobs.json'
+import { JobCard } from '../../componets/JobCard'
+import { JobListEmpty } from '@/componets/JobListEmpty'
+import { init } from 'next/dist/compiled/webpack/webpack'
 
 export const TrabajosRecientes = () => {
 
     //const [ isLogin, setIslogin ] = useState(false)
+    const [visible, setVisible] = useState(10)
+    const [initial, setInitial] = useState(0)
+
+    const updateList = () => {
+      setInitial( initial + 10 )
+      setVisible( visible + 10 )
+    }
+
+    const updateListPrevius = () => {
+      setInitial(initial - 10)
+      setVisible(visible - 10)
+    }
 
   return (
     <>
@@ -21,30 +35,58 @@ export const TrabajosRecientes = () => {
         </div>
         */}
         
-        <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5 p-4">
-          {
-            jobs.map( (item, index) => 
-              <JobCard
-                key={index}
-                destacado={item.destacado} 
-                imgPath={item.imagen}
-                titulo={item.titulo} 
-                descripcion={item.descripcion} 
-                categoria={item.categoria}
-                nombre={item.nombreFixer} 
-                apellido={item.apellidoFixer} 
-                ubicacion={item.ubicacion} 
-                tiempo={item.tiempoPublicado} 
-                calificacion={item.calificacion}
-                telefono={item.telefono}
-                precio={{
-                    min: item.precio.min,
-                    max: item.precio.max
-                  }} 
-              />
-            )
-          } 
+        {
+          jobs.length === 0 && 
+            <JobListEmpty /> 
+        }
+
+        {
+          jobs.length > 0 && 
+            <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5 p-4">
+              {
+                jobs.slice(initial, visible).map( (item, index) => 
+                (
+                  item.activo === true &&
+                    <JobCard
+                      key={index}
+                      destacado={item.destacado} 
+                      imgPath={item.imagen}
+                      titulo={item.titulo} 
+                      descripcion={item.descripcion} 
+                      categoria={item.categoria}
+                      nombre={item.nombreFixer} 
+                      apellido={item.apellidoFixer} 
+                      ubicacion={item.ubicacion} 
+                      tiempo={item.tiempoPublicado} 
+                      calificacion={item.calificacion}
+                      telefono={item.telefono}
+                      precio={{
+                          min: item.precio.min,
+                          max: item.precio.max
+                        }} 
+                    />
+                )
+                )
+              } 
+            </div>
+        }
+
+        <div className='flex flex-row justify-center gap-[5px]'>
+          <button
+            className='bg-[#d39625] hover:bg-[#1834c2] duration-150 text-white h-9 w-40 rounded-[8px]'
+            onClick={updateListPrevius}
+          >
+            Ver anteriores
+          </button>
+          <button 
+            className='bg-[#2585d3] hover:bg-[#1834c2] duration-150 text-white h-9 w-40 rounded-[8px]'
+            onClick={updateList}
+          > 
+            Ver mas 
+          </button> 
+
         </div>
+        
       </div>
     </>
   )
--- src/componets/JobCard.tsx
+++ src/componets/JobCard.tsx
@@ -23,18 +23,30 @@ interface Props {
     }
 }
 
+
 export const JobCard = ({destacado, imgPath, titulo, descripcion, categoria, nombre,apellido, ubicacion, tiempo, calificacion, telefono, precio}:Props) => {
     
     const [expandido, setExpandido] = useState(false)
 
     const {min, max} = precio
 
+    const handleClick = () => {
+        const phoneNumber = `${591}${telefono}`
+        const message = "Hola, quisiera mas informacion acerca del servicio"
+        const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`
+
+        window.open(url, "_blank")
+    }
+
   return (
     <>
-        <div className='flex flex-col rounded-[10px] justify-around border border-solid border-black/15 transition-shadow duration-300 hover:shadow-lg hover:shadow-black/30 mr-[5px] mb-[5px] p-3 min-h-[370] max-h-[370] max-w-[260] min-w-[260]'>
+        <div className={`flex flex-col rounded-[10px] justify-around border border-solid border-black/15 transition-shadow duration-300 hover:shadow-lg hover:shadow-black/30 mr-[5px] mb-[5px] p-3 min-h-[370] max-h-[370] max-w-[260] min-w-[260]`}>
             <div className='flex flex-row justify-between mb-[5px]'>
                 { 
-                    <span className={destacado ? `opacity-[100%] border border-solid pr-[5px] pl-[5px] text-[gold] rounded-[8px]` : `opacity-[0%]`}> Destacado </span> 
+                    <span className={destacado ? `opacity-[100%] border border-solid pr-[5px] pl-[5px] text-[gold] rounded-[8px]` : `opacity-[0%]`}
+                    > 
+                        Destacado 
+                    </span> 
                 } 
                 <span className={`border border-solid border-white/0 bg-[#a7f3a7] text-green-900 rounded-[10px] p-[1px] pr-[5px] pl-[5px] text-[13px] font-semibold `} > {categoria} </span>
             </div>
@@ -62,13 +74,21 @@ export const JobCard = ({destacado, imgPath, titulo, descripcion, categoria, nom
                 <span className='opacity-70'>{apellido}</span>
             </div>
 
-            <div className='flex items-center gap-[5px] opacity-70'>
-                <FiPhone />
-                <span> +591 {telefono} </span>
+            <div className='flex flex-row justify-between items-center gap-[5px] opacity-70'>
+                <div className='flex flex-row items-center'>
+                    <FiPhone />
+                    <span> +591 {telefono} </span>
+                </div>
+                <div>
+                    <span> Bs.{min}-{max} </span>
+                </div>
             </div>
 
             <div className='flex flex-row justify-between items-center'>
-                <button className='flex flex-row items-center justify-center gap-[3px] bg-[#25D366] hover:bg-[#128C7E] duration-150 text-white h-9 w-40 rounded-[8px]'>
+                <button 
+                    className='flex flex-row items-center justify-center gap-[3px] bg-[#25D366] hover:bg-[#128C7E] duration-150 text-white h-9 w-40 rounded-[8px]'
+                    onClick={handleClick}
+                >
                     <FaWhatsapp />
                     WhatsApp 
                 </button>
