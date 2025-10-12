ef746222f1972dcb165167ed8b489d99feb4ff4f
jason <201909851@est.umss.edu>
2025-10-12 10:16:23 -0400

    codigo de HU 5 jason

---
+++ src/app/servicios/page.tsx
@@ -0,0 +1,7 @@
+import ServicesGrid from "../components/hu5/ServicesGrid";
+
+export const metadata = { title: "Servicios | Servineo" };
+
+export default function ServiciosPage() {
+  return <ServicesGrid pageSize={12} />;
+}
