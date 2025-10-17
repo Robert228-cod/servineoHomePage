5806b767d522c3c6c49d722f4d4fbb4724013904
JAVI24-25 <201902218@est.umss.edu>
2025-10-17 17:49:15 -0400

    "agregacion de los popup que indica la ubicacion del usuario "

--- src/app/busqueda/components/map/UserMaker.tsx
+++ src/app/busqueda/components/map/UserMaker.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-import { Marker } from "react-leaflet";
+import { Marker, Popup } from "react-leaflet";
 
 interface UserMarkerProps {
   position: [number, number];
 }
 
 export default function UserMarker({ position }: UserMarkerProps) {
-  return <Marker position={position} />;
+  return (
+    <Marker position={position}>
+      <Popup>📍 Estás aquí</Popup>
+    </Marker>
+  );
 }
