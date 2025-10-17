053ba3c42168a8941963dd71fc3528d8fc586cfc
JAVI24-25 <201902218@est.umss.edu>
2025-10-17 17:34:12 -0400

    "inserciones de parametros opcionales radius, color" preparado para integrarse con controles COMPLETO

--- src/app/busqueda/components/map/MapCircle.tsx
+++ src/app/busqueda/components/map/MapCircle.tsx
@@ -5,24 +5,10 @@ import { Circle } from "react-leaflet";
 interface MapCircleProps {
   center: [number, number];
   radius?: number;
-  color?: string;
 }
 
-export default function MapCircle({
-  center,
-  radius = 5000,
-  color = "blue",
-}: MapCircleProps) {
-  return (
-    <Circle
-      center={center}
-      radius={radius}
-      pathOptions={{
-        color,
-        fillColor: color,
-        fillOpacity: 0.15,
-        weight: 1,
-      }}
-    />
-  );
+export default function MapCircle({ center, radius = 5000 }: MapCircleProps) {
+  return <Circle center={center} radius={radius} pathOptions={{ color: "blue", fillOpacity: 0.1 }} />;
 }
+
+
