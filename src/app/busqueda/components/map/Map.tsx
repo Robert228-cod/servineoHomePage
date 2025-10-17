d2ce9b6afe107e4bf665147244f71c5dc38fa810
JAVI24-25 <201902218@est.umss.edu>
2025-10-17 17:56:52 -0400

    agregando un marcador simple en la posicion por defecto y anade un popup

--- src/app/busqueda/components/map/Map.tsx
+++ src/app/busqueda/components/map/Map.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const defaultPosition: [number, number] = [-17.39381, -66.15693];
 
@@ -9,6 +9,9 @@ export default function Map() {
     <div style={{ height: "60vh", width: "100%" }}>
       <MapContainer center={defaultPosition} zoom={14} style={{ height: "100%", width: "100%" }}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={defaultPosition}>
+          <Popup>📍 Posición inicial</Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
