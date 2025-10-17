5d45db80df1affaee2be41f33b022c9ae2013fa4
JAVI24-25 <201902218@est.umss.edu>
2025-10-17 17:44:16 -0400

    " solucioando el problema para evitar que el mapa se recoloque si la poscion cambia"

--- src/app/busqueda/components/map/RecenterMap.tsx
+++ src/app/busqueda/components/map/RecenterMap.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useMap } from "react-leaflet";
 
 interface RecenterMapProps {
@@ -8,8 +8,18 @@ interface RecenterMapProps {
 
 export default function RecenterMap({ position }: RecenterMapProps) {
   const map = useMap();
+  const prevPosition = useRef<[number, number] | null>(null);
 
   useEffect(() => {
+    if (
+      prevPosition.current &&
+      prevPosition.current[0] === position[0] &&
+      prevPosition.current[1] === position[1]
+    ) {
+      return; // no mover si no cambió
+    }
+
+    prevPosition.current = position;
     map.flyTo(position, map.getZoom(), { animate: true, duration: 0.8 });
   }, [position, map]);
 
